b3985da06769a43d0ceff8aa62a281a7bf4d06f1
Victor Hosth <kvbn147@gmail.com>
2023-03-14 12:49:36 +0000

    verifica se a query fornecida contém um pedido válido na dataBase Olders!

--- src/controllers/olderController.ts
+++ src/controllers/olderController.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from "express";
+import * as dataBase from "../models/dataBase";
 import { dataOlders, dataClient } from "../models/filtersData"
 
 export const olders = (req: Request, res: Response) => {
     let query = req.query.id as string;
     let queryNumber: number = parseInt(query);
-    let objOlder = dataOlders.getFromID(queryNumber);
-    let clientId = objOlder?.customer_id as number;
-    let objClient = dataClient.getFromID(clientId);
 
-    let arraySend: older[] = objOlder?.products as older[];
+    if (queryNumber >= 1 && queryNumber <= dataBase.olders.length) {
+        let objOlder = dataOlders.getFromID(queryNumber);
+        let clientId = objOlder?.customer_id as number;
+        let objClient = dataClient.getFromID(clientId);
 
-    res.render('pages/olders', {
-        olders: objClient,
-        test: dataOlders.getArrayOlders(arraySend)
-    })
+        let arraySend: older[] = objOlder?.products as older[];
+        res.render('pages/olders', {
+            olders: objClient,
+            test: dataOlders.getArrayOlders(arraySend)
+        })
+    } else {
+        res.render('pages/olders', {
+
+        })
+    }
+
+
+
 }
 
 
